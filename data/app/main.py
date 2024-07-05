d62460ffd6bb7fc0c157ea8d513b0831128069e3
ValorenceCLE <landon.bell@valorence.com>
2024-07-05 14:17:51 -0600

    main

--- data/app/main.py
+++ data/app/main.py
@@ -3,11 +3,13 @@ from rp import INA260Router
 from env import AHT10
 from net import ping_test #Needs root permissions to run
 from cell import cell
+from system_info import save
 env = AHT10()
 cp = INA260Camera()
 rp = INA260Router()
 
 def main():
+    save()
     rp.rp_test()
     cp.cp_test()
     env.env_test()
