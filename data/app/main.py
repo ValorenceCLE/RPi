2267ac84733aa8f4bf5c304ae323c3c8fc5fc6b5
ValorenceCLE <landon.bell@valorence.com>
2024-07-09 09:04:31 -0600

    main

--- data/app/main.py
+++ data/app/main.py
@@ -4,7 +4,7 @@ from env import AHT10
 from net import NetworkPingTest
 from cell import cell
 from system_info import save
-env = AHT10()
+#env = AHT10()
 cp = INA260Camera()
 rp = INA260Router()
 net = NetworkPingTest()
@@ -12,7 +12,7 @@ def main():
     save()
     rp.rp_run()
     cp.cp_run()
-    env.env_run()
+    #env.env_run()
     cell()
     net.net_run()
         
