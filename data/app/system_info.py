7699ada39fe621dd5a2dcf5aee8e4e8a884a845d
ValorenceCLE <landon.bell@valorence.com>
2024-07-16 15:50:03 -0600

    pysnmp

--- data/app/system_info.py
+++ data/app/system_info.py
@@ -1,6 +1,6 @@
 import json
 import os
-from pysnmp.hlapi import *
+from pysnmp.hlapi.asyncio import SnmpEngine, CommunityData, UdpTransportTarget, ContextData, ObjectType, ObjectIdentity, getCmd
 import logging
 
 MODEL_OID = '.1.3.6.1.2.1.1.1.0'
