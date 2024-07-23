63d32179ef9d5497ab3d7c09f9e48528b5db02c0
ValorenceCLE <landon.bell@valorence.com>
2024-07-23 08:37:28 -0600

    60seconds

--- data/app/listener.py
+++ data/app/listener.py
@@ -21,7 +21,7 @@ class SaveData:
         self.redis = Redis.from_url(self.redis_url)
         self.group_name = 'data_group'
         self.consumer_name = 'influxdb'
-        self.collection_interval = 30  # Pull data every 30 seconds
+        self.collection_interval = 60  # Pull data every 30 seconds
         
         
     async def setup_groups(self, streams):
