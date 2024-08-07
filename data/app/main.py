90ed15955af33a0ed247fce17138ba9ec4b14094
ValorenceCLE <landon.bell@valorence.com>
2024-08-07 09:38:38 -0600

    Cell OFF

--- data/app/main.py
+++ data/app/main.py
@@ -4,18 +4,18 @@ from rp import INA260Router
 #from sp import INA260System
 from env import AHT10
 from net import NetworkPing
-from cell import CellularMetrics
+#from cell import CellularMetrics
 from system_info import start_up
 from listener import Processor
 
 async def main():
     net = NetworkPing() 
-    cell = CellularMetrics()
+    #cell = CellularMetrics()
     cp = INA260Camera()
     rp = INA260Router()
     #sp = INA260System()
     env = AHT10()
-    streams = ['network_data', 'camera_data', 'router_data', 'environment_data', 'cellular_data'] #List the Streams that need to be listened too, pass var to the Processor
+    streams = ['network_data', 'camera_data', 'router_data', 'environment_data'] #List the Streams that need to be listened too, pass var to the Processor
     prc = Processor(streams) #Saves Data to InfluxDB 
     
     
@@ -26,7 +26,7 @@ async def main():
         asyncio.create_task(rp.run()),
         #asyncio.create_task(sp.run()), #Currently No Sensor Connected
         asyncio.create_task(env.run()),
-        asyncio.create_task(cell.cell_run()), #Currently No SIM
+        #asyncio.create_task(cell.cell_run()), #Currently No SIM
         asyncio.create_task(prc.process_streams())
     ]
     
