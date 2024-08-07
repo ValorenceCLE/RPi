7528d6115f3ee011a845c8939c08620b651aaae5
ValorenceCLE <landon.bell@valorence.com>
2024-08-07 09:29:31 -0600

    Cell Collection ON

--- data/app/main.py
+++ data/app/main.py
@@ -4,13 +4,13 @@ from rp import INA260Router
 #from sp import INA260System
 from env import AHT10
 from net import NetworkPing
-#from cell import CellularMetrics
+from cell import CellularMetrics
 from system_info import start_up
 from listener import Processor
 
 async def main():
     net = NetworkPing() 
-    #cell = CellularMetrics()
+    cell = CellularMetrics()
     cp = INA260Camera()
     rp = INA260Router()
     #sp = INA260System()
@@ -26,7 +26,7 @@ async def main():
         asyncio.create_task(rp.run()),
         #asyncio.create_task(sp.run()), #Currently No Sensor Connected
         asyncio.create_task(env.run()),
-        #asyncio.create_task(cell.cell_run()), #Currently No SIM
+        asyncio.create_task(cell.cell_run()), #Currently No SIM
         asyncio.create_task(prc.process_streams())
     ]
     
