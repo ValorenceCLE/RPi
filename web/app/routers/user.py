d818ecbde0d3211190102da5ac7d042f3c99f001
ValorenceCLE <landon.bell@valorence.com>
2024-12-17 10:12:35 -0700

    Update Testing

--- web/app/routers/user.py
+++ web/app/routers/user.py
@@ -26,9 +26,4 @@ async def help_page(request: Request, user: dict = Depends(get_current_user)):
 # Testing 
 @router.get("/test")
 async def test(request: Request, user: dict = Depends(get_current_user)):
-    return {"message": "Hey Pal, Go Fuck Yourself!!!!"}
-
-
-@router.get("/testing")
-async def test(request: Request, user: dict = Depends(get_current_user)):
-    return {"message": "Hey Pal, Go Fuck Yourself! One More Time"}
+    return {"message": "Hey Pal, Go Fuck Yourself!!!! :)"}
